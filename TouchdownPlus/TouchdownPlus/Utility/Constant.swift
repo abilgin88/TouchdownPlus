43c7afe55143783bcfbd869d11ed9f0f9c87a079
Abdullah Bilgin <abilgin02@icloud.com>
2023-09-08 19:23:36 +0300

    Creating a constant utility for project

---
+++ TouchdownPlus/TouchdownPlus/Utility/Constant.swift
@@ -0,0 +1,29 @@
+//
+//  Constant.swift
+//  TouchdownPlus
+//
+//  Created by Abdullah Bilgin on 9/8/23.
+
+import SwiftUI
+
+// Data
+
+// Color
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// Layout
+
+// Ux
+
+// Api
+
+// Image
+
+// Font
+
+// String
+
+// Misc
+
+ 

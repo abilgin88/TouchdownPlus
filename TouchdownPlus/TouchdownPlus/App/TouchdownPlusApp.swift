30e99dbea2704c166ce0788335ad99781b36784a
Abdullah Bilgin <abilgin02@icloud.com>
2023-09-08 12:00:19 +0300

    Project setup (icon, color image etc.), file organization and info apperance and oriantation and layout

---
+++ TouchdownPlus/TouchdownPlus/App/TouchdownPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownPlusApp.swift
+//  TouchdownPlus
+//
+//  Created by Abdullah Bilgin on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
